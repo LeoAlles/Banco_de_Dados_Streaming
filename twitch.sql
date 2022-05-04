83f62a8124a2fa812c4f57834c5334a09491e988
Leonardo Alles <leonardoralles@gmail.com>
2022-05-04 19:26:33 -0300

    refactor

--- twitch.sql
+++ twitch.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS Usuarios,Usuariosprime,CriadoresParceirosdaTwitch,Emotes,Transmissao,Inscricao,Clipes,Anunciante,Anuncio,MensagemChat,Categorizacao,Categorias,Tags,Sussurro,Segue,Contem,Rotulacao,Cheer;
+DROP TABLE IF EXISTS Usuarios,Usuariosprime,CriadoresParceirosdaTwitch,Emotes,Transmissao,Inscricao,Clipes,Anunciante,Anuncio,MensagemChat,Categorizacao,Categorias,Tags,Sussurro,Segue,Contem,Rotulacao,Cheer,Visualizacao;
 
 CREATE TABLE Usuarios (
     email VARCHAR(50) NOT NULL,
@@ -10,26 +10,10 @@ CREATE TABLE Usuarios (
   	PRIMARY KEY (nomeUsuario)
 );
 
-CREATE TABLE UsuariosPrime (
-    fk_Usuarios_nomeUsuario VARCHAR(20) PRIMARY KEY NOT NULL,
-  	FOREIGN KEY (fk_Usuarios_nomeUsuario) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE
-);
-   
-
 CREATE TABLE CriadoresParceirosdaTwitch (
     saldoDinheiro INTEGER,
-    fk_Usuarios_nomeUsuario VARCHAR(20) PRIMARY KEY NOT NULL,
-  	FOREIGN KEY (fk_Usuarios_nomeUsuario) REFERENCES Usuarios(nomeUsuario) ON DELETE CASCADE
-);
-
-
-CREATE TABLE Transmissao (
-    Inicio TIMESTAMP PRIMARY KEY NOT NULL,
-  	Fim TIMESTAMP NOT NULL,
-    numeroDeEspectadores INTEGER NOT NULL,
-  	nome VARCHAR(40) NOT NULL,
-  	fk_Usuarios_nomeUsuario VARCHAR(20) NOT NULL,
-    FOREIGN KEY (fk_Usuarios_nomeUsuario) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE
+    criadorParceiro VARCHAR(20) PRIMARY KEY NOT NULL,
+  	FOREIGN KEY (criadorParceiro) REFERENCES Usuarios(nomeUsuario) ON DELETE CASCADE
 );
 
 CREATE TABLE Inscricao (
@@ -37,19 +21,35 @@ CREATE TABLE Inscricao (
     inicio DATE NOT NULL,
     fim DATE NOT NULL,
     preco INTEGER NOT NULL,
-    fk_Usuarios_nomeUsuario VARCHAR(20) NOT NULL,
-    fk_CriadoresParceirosdaTwitch_fk_Usuarios_nomeUsuario VARCHAR(20) NOT NULL,
-    fk_UsuariosPrime_fk_Usuarios_nomeUsuario VARCHAR(20) NOT NULL,
-  	FOREIGN KEY (fk_Usuarios_nomeUsuario) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE,
-  	FOREIGN KEY (fk_CriadoresParceirosdaTwitch_fk_Usuarios_nomeUsuario) REFERENCES CriadoresParceirosdaTwitch (fk_Usuarios_nomeUsuario) ON DELETE CASCADE
+  	criadorParceiro VARCHAR(20) not NULL,
+    nomeUsuario VARCHAR(20),
+  	FOREIGN KEY (criadorParceiro) REFERENCES CriadoresParceirosdaTwitch(criadorParceiro) ON DELETE CASCADE,
+  	FOREIGN KEY (nomeUsuario) REFERENCES Usuarios(nomeUsuario) ON DELETE CASCADE
 );
 
+CREATE TABLE UsuariosPrime (
+    nomeUsuarioPrime VARCHAR(20) PRIMARY KEY NOT NULL,
+  	idInscricaoPrime CHAR(12),
+  	FOREIGN KEY (nomeUsuarioPrime) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE,
+  	FOREIGN Key (idInscricaoPrime) REFERENCES Inscricao(idInscricao) on DELETE set NULL
+);
+   
+CREATE TABLE Transmissao (
+  	idTransmissao char(12) PRIMARY KEY not NULL,
+    Inicio TIMESTAMP NOT NULL,
+  	Fim TIMESTAMP NOT NULL,
+  	nome VARCHAR(40) NOT NULL,
+  	criador VARCHAR(20) NOT NULL,
+    FOREIGN KEY (criador) REFERENCES Usuarios(nomeUsuario) ON DELETE CASCADE
+);
+
+
 CREATE TABLE Emotes (
-    NomeEmote VARCHAR(20) NOT NULL,
+    nomeEmote VARCHAR(20) NOT NULL,
     imagem VARCHAR(50) NOT NULL,
-    fk_Inscricao_idInscricao CHAR(12),
-  	PRIMARY KEY(fk_Inscricao_idInscricao,NomeEmote),
-  	FOREIGN KEY (fk_Inscricao_idInscricao) REFERENCES Inscricao (idInscricao) ON DELETE CASCADE
+    idInscricao CHAR(12),
+  	PRIMARY KEY(idInscricao,NomeEmote),
+  	FOREIGN KEY (idInscricao) REFERENCES Inscricao(idInscricao) ON DELETE CASCADE
 );
 
 
@@ -58,97 +58,102 @@ CREATE TABLE Clipes (
     idClipe CHAR(12) PRIMARY KEY NOT NULL,
   	inicio 	INTEGER NOT NULL,
     visualizacoesDoClipe INTEGER NOT NULL,
-    fk_Usuarios_nomeUsuario VARCHAR(20) NOT NULL,
-  	fk_Transmissao_Inicio TIMESTAMP NOT NULL,
-  	FOREIGN KEY (fk_Usuarios_nomeUsuario) REFERENCES Usuarios(nomeUsuario) ON DELETE CASCADE,
-    FOREIGN KEY (fk_Transmissao_Inicio) REFERENCES Transmissao (Inicio) ON DELETE CASCADE
+    nomeUsuario VARCHAR(20) NOT NULL,
+  	idTransmissao char(12) NOT NULL,
+  	FOREIGN KEY (nomeUsuario) REFERENCES Usuarios(nomeUsuario) ON DELETE CASCADE,
+    FOREIGN KEY (idTransmissao) REFERENCES Transmissao(idTransmissao) ON DELETE CASCADE
 );
 
 CREATE TABLE Anunciante (
-    NomeEmpresa VARCHAR(20) PRIMARY KEY NOT NULL
+    nomeEmpresa VARCHAR(20) PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE Anuncio (
     numeroAnuncio INTEGER NOT NULL,
     video VARCHAR(50)NOT NULL,
-    fk_Anunciante_NomeEmpresa VARCHAR(20) NOT NULL,
-  	FOREIGN KEY(fk_Anunciante_NomeEmpresa) REFERENCES Anunciante (NomeEmpresa)ON DELETE CASCADE,
+    nomeEmpresa VARCHAR(20) NOT NULL,
+  	FOREIGN KEY(nomeEmpresa) REFERENCES Anunciante(nomeEmpresa)ON DELETE CASCADE,
   	PRIMARY KEY(numeroAnuncio),
-  	UNIQUE(fk_Anunciante_NomeEmpresa,numeroAnuncio)
+  	UNIQUE(nomeEmpresa,numeroAnuncio)
 );
     
 CREATE TABLE MensagemChat (
     texto VARCHAR(100) NOT NULL,
-    msg_timestamp timestamp NOT NULL,
-    fk_Transmissao_Inicio TIMESTAMP NOT NULL,
-    fk_Usuarios_nomeUsuario varchar(20) NOT NULL,
-  	PRIMARY KEY(fk_Usuarios_nomeUsuario,msg_timestamp),
-  	FOREIGN KEY (fk_Transmissao_Inicio) REFERENCES Transmissao (Inicio) ON DELETE CASCADE,
-  	FOREIGN KEY (fk_Usuarios_nomeUsuario) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADe
+    msgTimestamp timestamp NOT NULL,
+    idTransmissao char(12) NOT NULL,
+    nomeUsuario varchar(20) NOT NULL,
+  	PRIMARY KEY(nomeUsuario,msgTimestamp),
+  	FOREIGN KEY (idTransmissao) REFERENCES Transmissao(idTransmissao) ON DELETE CASCADE,
+  	FOREIGN KEY (nomeUsuario) REFERENCES Usuarios(nomeUsuario) ON DELETE CASCADe
 );
 
 
 CREATE TABLE Categorias (
-    nome VARCHAR(25) PRIMARY KEY NOT NULL
+    nomeCategoria VARCHAR(25) PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE Tags (
-    nome VARCHAR(25) PRIMARY KEY NOT NULL
+    nomeTag VARCHAR(25) PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE Segue (
-    fk_Usuarios_nomeUsuario VARCHAR(20) NOT NULL,
-    fk_Usuarios_nomeUsuario_ VARCHAR(20) NOT NULL,
-  	PRIMARY KEY(fk_Usuarios_nomeUsuario,fk_Usuarios_nomeUsuario_),
-    FOREIGN KEY (fk_Usuarios_nomeUsuario) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE,
-  	FOREIGN KEY (fk_Usuarios_nomeUsuario_) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE
+    nomeUsuarioSegue VARCHAR(20) NOT NULL,
+    nomeUsuarioSeguido VARCHAR(20) NOT NULL,
+  	PRIMARY KEY(nomeUsuarioSegue, nomeUsuarioSeguido),
+    FOREIGN KEY (nomeUsuarioSegue) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE,
+  	FOREIGN KEY (nomeUsuarioSeguido) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE
 );
 
 
 
 CREATE TABLE Sussurro (
-    fk_Usuarios_nomeUsuario VARCHAR(20) NOT NULL,
-    fk_Usuarios_nomeUsuario_ VARCHAR(20) NOT NULL,
+    nomeUsuarioManda VARCHAR(20) NOT NULL,
+    nomeUsuarioRecebe VARCHAR(20) NOT NULL,
     horario TIMESTAMP NOT NULL,
     texto VARCHAR(100) NOT NULL,
-  	PRIMARY KEY(fk_Usuarios_nomeUsuario,fk_Usuarios_nomeUsuario_,horario),
-    FOREIGN KEY (fk_Usuarios_nomeUsuario) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE,
-  	FOREIGN KEY (fk_Usuarios_nomeUsuario_) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE
+  	PRIMARY KEY(nomeUsuarioManda,nomeUsuarioRecebe,horario),
+    FOREIGN KEY (nomeUsuarioManda) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE,
+  	FOREIGN KEY (nomeUsuarioRecebe) REFERENCES Usuarios (nomeUsuario) ON DELETE CASCADE
 );
 
 CREATE TABLE Contem (
-    fk_Transmissao_Inicio TIMESTAMP NOT NULL,
-    fk_Anuncio_numeroAnuncio INTEGER NOT NULL,
-  	fk_Usuarios_nomeUsuario VARCHAR(20) NOT NULL,
-  	PRIMARY KEY(fk_Transmissao_Inicio,fk_Anuncio_numeroAnuncio,fk_Usuarios_nomeUsuario),
-    FOREIGN KEY (fk_Transmissao_Inicio) REFERENCES Transmissao (inicio) ON DELETE SET NULL,
-  	FOREIGN KEY (fk_Anuncio_numeroAnuncio) REFERENCES Anuncio (numeroAnuncio) ON DELETE SET NULL
+    idTransmissao char(12) NOT NULL,
+    numeroAnuncio INTEGER NOT NULL,
+  	PRIMARY KEY(idTransmissao,numeroAnuncio),
+    FOREIGN KEY (idTransmissao) REFERENCES Transmissao(idTransmissao) ON DELETE SET NULL,
+  	FOREIGN KEY (numeroAnuncio) REFERENCES Anuncio(numeroAnuncio) ON DELETE SET NULL
 );
 
 CREATE TABLE Categorizacao (
-    fk_Transmissao_Inicio TIMESTAMP NOT NULL,
-    fk_Categorias_nome VARCHAR(25) NOT NULL,
-  	fk_Usuarios_nomeUsuario VARCHAR(20) NOT NULL,
-  	PRIMARY KEY(fk_Transmissao_Inicio, fk_Categorias_nome, fk_Usuarios_nomeUsuario),
-  	FOREIGN KEY (fk_Transmissao_Inicio) REFERENCES Transmissao (inicio) ON DELETE SET NULL
+    idTransmissao char(12) NOT NULL,
+   	nomeCategoria VARCHAR(25) NOT NULL,
+  	PRIMARY KEY(idTransmissao,nomeCategoria),
+  	FOREIGN KEY (idTransmissao) REFERENCES Transmissao(idTransmissao) ON DELETE SET NULL
 );
 
 CREATE TABLE Rotulacao (
-    fk_Tags_nome VARCHAR(25) NOT NULL,
-    fk_Categorias_nome VARCHAR(25) NOT NULL,
-  	PRIMARY KEY(fk_Tags_nome,fk_Categorias_nome),
-  	FOREIGN KEY (fk_Tags_nome) REFERENCES Tags (nome) ON DELETE SET NULL,
-  	FOREIGN KEY (fk_Categorias_nome) REFERENCES Categorias (nome)
+    nomeTag VARCHAR(25) NOT NULL,
+    nomeCategoria VARCHAR(25) NOT NULL,
+  	PRIMARY KEY(nomeTag,nomeCategoria),
+  	FOREIGN KEY (nomeTag) REFERENCES Tags (nomeTag) ON DELETE SET NULL,
+  	FOREIGN KEY (nomeCategoria) REFERENCES Categorias (nomeCategoria)
 );
 
-CREATE TABLE Cheer (
-    fk_Usuarios_nomeUsuario VARCHAR(20) NOT NULL,
-    fk_CriadoresParceirosdaTwitch_fk_Usuarios_nomeUsuario VARCHAR(20) NOT NULL,
+CREATE TABLE Cheer(
+    nomeUsuario VARCHAR(20) NOT NULL,
+    criadorParceiro VARCHAR(20) NOT NULL,
     numerodebits INTEGER NOT NULL,
     horario TIMESTAMP NOT NULL,
-  	PRIMARY KEY(fk_Usuarios_nomeUsuario,fk_CriadoresParceirosdaTwitch_fk_Usuarios_nomeUsuario,horario),
-  	FOREIGN KEY (fk_Usuarios_nomeUsuario) REFERENCES Usuarios (nomeUsuario) ON DELETE RESTRICT,
-  	FOREIGN KEY (fk_CriadoresParceirosdaTwitch_fk_Usuarios_nomeUsuario) REFERENCES CriadoresParceirosdaTwitch (fk_Usuarios_nomeUsuario) ON DELETE SET NULL
+  	PRIMARY KEY(nomeUsuario,criadorParceiro,horario),
+  	FOREIGN KEY (nomeUsuario) REFERENCES Usuarios(nomeUsuario) ON DELETE RESTRICT,
+  	FOREIGN KEY (criadorParceiro) REFERENCES CriadoresParceirosdaTwitch(criadorParceiro) ON DELETE SET NULL
+);
+
+CREATE TABLE Visualizacao(
+	nomeUsuario VARCHAR(20) NOT NULL,
+ 	idTransmissao char(12),
+    FOREIGN KEY (nomeUsuario) REFERENCES Usuarios(nomeUsuario) ON DELETE set NULL,
+  	FOREIGN KEY (idTransmissao) REFERENCES Transmissao(idTransmissao) on DELETE cascade
 );
 
 INSERT INTO Usuarios VALUES('email1@email.com',0,'User1','2001-05-01','123456789','Sou um user da Twitch');
@@ -158,17 +163,17 @@ INSERT INTO Usuarios VALUES('email4@email.com',4,'User4','2002-12-01','123456789
 INSERT INTO Usuarios VALUES('email5@email.com',19,'User5','2001-11-21','123456789','Sou um user da Twitch');
 INSERT INTO Usuarios VALUES('email6@email.com',4,'User6','2009-12-31','123456789','Sou um user da Twitch');
 
-INSERT INTO UsuariosPrime VALUES('User4');
-INSERT INTO UsuariosPrime VALUES('User5');
-INSERT INTO UsuariosPrime VALUES('User6');
-
 INSERT INTO CriadoresParceirosdaTwitch VALUES(10,'User4');
 INSERT INTO CriadoresParceirosdaTwitch VALUES(14,'User5');
 INSERT INTO CriadoresParceirosdaTwitch VALUES(20,'User6');
- 
-INSERT INTO Inscricao VALUES('123456789012', '2009-12-31','2009-12-31', 10, 'User5', 'User4', 'User5');
-INSERT INTO Inscricao VALUES('123456789013', '2009-12-31','2009-12-31', 10, 'User6', 'User4', 'User6');
-INSERT INTO Inscricao VALUES('123456789014', '2009-12-31','2009-12-31', 10, 'User1', 'User5', 'User4');
+
+INSERT INTO Inscricao VALUES('123456789012', '2009-12-31','2009-12-31', 10, 'User5', 'User1');
+INSERT INTO Inscricao VALUES('123456789013', '2009-12-31','2009-12-31', 10, 'User6', 'User4');
+INSERT INTO Inscricao VALUES('123456789014', '2009-12-31','2009-12-31', 10, 'User4', 'User3');
+
+INSERT INTO UsuariosPrime VALUES('User4','123456789012');
+INSERT INTO UsuariosPrime VALUES('User5','123456789014');
+INSERT INTO UsuariosPrime VALUES('User6');
 
 INSERT INTO Segue VALUES('User1', 'User2');
 INSERT INTO Segue VALUES('User2', 'User3');
@@ -178,13 +183,13 @@ INSERT INTO Emotes VALUES('carinha feliz','emotes/img1','123456789012');
 INSERT INTO Emotes VALUES('carinha triste','emotes/img2','123456789013');
 INSERT INTO Emotes VALUES('bola de futebol','emotes/img3','123456789014');
  
-INSERT INTO Transmissao VALUES('2004-10-19 10:23:54','2004-10-19 10:54:54',124,'Transmissao 1', 'User5');
-INSERT INTO Transmissao VALUES('2004-10-19 10:23:57','2004-10-19 11:54:54',124,'Transmissao 2', 'User6');
-INSERT INTO Transmissao VALUES('2004-10-19 11:23:54','2004-10-19 16:54:54',124,'Transmissao 3', 'User4');
+INSERT INTO Transmissao VALUES('123456789011','2004-10-19 10:23:54','2004-10-19 10:54:54','Transmissao 1', 'User5');
+INSERT INTO Transmissao VALUES('123456789012','2004-10-19 10:23:57','2004-10-19 11:54:54','Transmissao 2', 'User6');
+INSERT INTO Transmissao VALUES('123456789013','2004-10-19 11:23:54','2004-10-19 16:54:54','Transmissao 3', 'User4');
 
-INSERT INTO Clipes VALUES('142', '123456789012', 543, 12, 'User1','2004-10-19 10:23:54');
-INSERT INTO Clipes VALUES('156', '123456789013', 1543, 16, 'User1','2004-10-19 10:23:54');
-INSERT INTO Clipes VALUES('172', '123456789015', 5441, 21, 'User1','2004-10-19 10:23:54');
+INSERT INTO Clipes VALUES('142', '123456789012', 543, 12, 'User1','123456789011');
+INSERT INTO Clipes VALUES('156', '123456789013', 1543, 16, 'User1','123456789012');
+INSERT INTO Clipes VALUES('172', '123456789015', 5441, 21, 'User1','123456789012');
 
 INSERT INTO Anunciante VALUES('DELL');
 INSERT INTO Anunciante VALUES('INTEL');
@@ -194,9 +199,9 @@ INSERT INTO Anuncio VALUES(4,'anuncios/video1', 'DELL');
 INSERT INTO Anuncio VALUES(5,'anuncios/video2', 'DELL');
 INSERT INTO Anuncio VALUES(1,'anuncios/video3', 'APPLE');
 
-INSERT INTO MensagemChat VALUES('Ola, que dia bonito', '2004-10-19 11:23:54', '2004-10-19 10:23:57', 'User1');
-INSERT INTO MensagemChat VALUES('Como vai?', '2004-11-19 10:23:56', '2004-10-19 10:23:57', 'User2');
-INSERT INTO MensagemChat VALUES('Vou bem', '2004-11-19 10:23:59', '2004-10-19 10:23:57', 'User1');
+INSERT INTO MensagemChat VALUES('Ola, que dia bonito', '2004-10-19 11:23:54', '123456789011', 'User1');
+INSERT INTO MensagemChat VALUES('Como vai?', '2004-11-19 10:23:56', '123456789013', 'User2');
+INSERT INTO MensagemChat VALUES('Vou bem', '2004-11-19 10:23:59', '123456789013', 'User1');
 
 INSERT INTO Categorias VALUES ('CSGO');
 INSERT INTO Categorias VALUES ('Esporte');
@@ -231,6 +236,4 @@ SELECT * FROM MensagemChat;
 SELECT * FROM cheer;
 SELECT * FROM Categorias;
 SELECT * FROM Sussurro;
-SELECT * FROM Tags;    
-    
-
+SELECT * FROM Tags;   
